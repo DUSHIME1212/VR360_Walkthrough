ea571e822fa3d3910d3efaff014365dd55da7799
DUSHIME1212 <mdonavan33@gmail.com>
2026-02-03 10:33:59 +0300

    fx: adding tags

--- Assets/Scripts/Location/Location.cs
+++ Assets/Scripts/Location/Location.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 
-public class Location : MonoBehaviour
+namespace VRCampusTour.Location
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// Base location component - rename this if not needed
+    /// </summary>
+    public class LocationComponent : MonoBehaviour
     {
-        
-    }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
     }
 }
--- Assets/Scripts/Location/LocationData.cs
+++ Assets/Scripts/Location/LocationData.cs
@@ -11,8 +11,8 @@ namespace VRCampusTour.Location
         public string description;
         public Material skyboxMaterial;
         public AudioClip ambienceAudio;
-        public List<HotspotData> infoHotspots;
-        public List<NavigationData> navigationPoints;
+        public List<HotspotData> infoHotspots = new List<HotspotData>();
+        public List<NavigationData> navigationPoints = new List<NavigationData>();
     }
 
     [Serializable]
@@ -40,42 +40,3 @@ namespace VRCampusTour.Location
         public List<LocationData> locations = new List<LocationData>();
     }
 }
-// using System;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// [Serializable]
-// public class LocationData
-// {
-//     public string locationName;
-//     public string description;
-//     public Material skyboxMaterial;
-//     public AudioClip ambienceAudio;
-//     public List<HotspotData> infoHotspots;
-//     public List<NavigationData> navigationPoints;
-// }
-
-// [Serializable]
-// public class HotspotData
-// {
-//     public string title;
-//     [TextArea(3, 6)]
-//     public string information;
-//     public Vector3 worldPosition;
-//     public Sprite icon;
-// }
-
-// [Serializable]
-// public class NavigationData
-// {
-//     public string destinationName;
-//     public int destinationLocationIndex;
-//     public Vector3 worldPosition;
-//     public Sprite previewImage;
-// }
-
-// [CreateAssetMenu(fileName = "NewLocation", menuName = "VR Campus Tour/Location Data")]
-// public class LocationDataSO : ScriptableObject
-// {
-//     public List<LocationData> locations = new List<LocationData>();
-// }
